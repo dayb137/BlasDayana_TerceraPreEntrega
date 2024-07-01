be3dd7a4dc7e79c2c58986d17c033d88147e24e2
day <dayblas007@gmail.com>
2024-07-01 10:27:39 -0300

    corregi errores de identacion

--- inicio/form.py
+++ inicio/form.py
@@ -4,4 +4,5 @@ class CrearPersonaFormulario(forms.Form):
     nombre = forms.CharField(max_length=30)
     apellido = forms.CharField(max_length=30)
     
-
+class BuscarPersona(forms.Form):
+    apellido = forms.CharField(max_length=30, required=False)
--- inicio/views.py
+++ inicio/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render, redirect
 from inicio.models import Persona
-from inicio.form import CrearPersonaFormulario
+from inicio.form import CrearPersonaFormulario, BuscarPersona
 
 
 def inicio(request):
     return render(request,'inicio/inicio.html')
 
 def crear_persona(request):
+    
+    formulario = CrearPersonaFormulario()
    
     if request.method == 'POST':
-        
         formulario = CrearPersonaFormulario(request.POST)
-        
         if formulario.is_valid():
             datos = formulario.cleaned_data
             persona = Persona(nommbre=datos.get('nombre'), apellido=datos.get('apellido'))
-            persona.save()
-            
-        return redirect('Personas')
+            persona.save()   
+            return redirect('personas')
     
-    else:
-        formulario = CrearPersonaFormulario()   
                     
-    return render(request,'inicio/crear.html', {'formulario':formulario})
+    return render(request,'inicio/crear_persona.html', {'formulario':formulario})
        
     
 def personas(request):
     
-    personas = Persona.objects.all()
+    formulario = BuscarPersona(request.GET)
     
-    return render(request, 'inicio/personas.html', {'personas':personas})
+    if formulario.is_valid():
+        apellido = formulario.cleaned_data['apellido']
+        personas = Persona.objects.filter(apellido__icontains=apellido)
+    
+    
+    return render(request, 'inicio/personas.html', {'personas':personas , 'formulario': formulario})
